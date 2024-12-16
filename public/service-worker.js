6ac0e00b43dfae7baf047f9b1bd58ed5615db502
Jun Gil <145642451+jungil13@users.noreply.github.com>
2024-12-16 21:46:23 +0800

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-  });
-  
+  event.waitUntil(
+    caches.open('static-v1').then((cache) => {
+      return cache.addAll([
+        '/image.png', // local resources
+        '/img/icons/tumblr_image.gif' // local version of the image
+      ]);
+    })
+  );
+});
   self.addEventListener('fetch', (event) => {
     console.log('Service Worker: Fetching', event.request.url);
   });
@@ -10,7 +16,9 @@ self.addEventListener('install', (event) => {
       caches.open('static-v1').then((cache) => {
         return cache.addAll([
           '/image.png', // local resources
-          'https://24.media.tumblr.com/d2780fd4d3e24bef0070f0a90537062b/tumblr_mpyzh7MK9J1rs8ziuo1_500.gif' // external resource
+          new Request('https://24.media.tumblr.com/d2780fd4d3e24bef0070f0a90537062b/tumblr_mpyzh7MK9J1rs8ziuo1_500.gif', {
+            mode: 'no-cors', // This bypasses the CORS policy
+          })
         ]);
       })
     );
