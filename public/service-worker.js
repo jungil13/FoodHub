1de84b83031df8567534ccffee8e02805e101286
Jun Gil <145642451+jungil13@users.noreply.github.com>
2024-12-16 21:37:50 +0800

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,4 +5,14 @@ self.addEventListener('install', (event) => {
   self.addEventListener('fetch', (event) => {
     console.log('Service Worker: Fetching', event.request.url);
   });
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('static-v1').then((cache) => {
+        return cache.addAll([
+          '/image.png', // local resources
+          'https://24.media.tumblr.com/d2780fd4d3e24bef0070f0a90537062b/tumblr_mpyzh7MK9J1rs8ziuo1_500.gif' // external resource
+        ]);
+      })
+    );
+  });
   
